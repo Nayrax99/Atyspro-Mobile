4915e2556dc6cc433f46c8b9d2147d6cb897f71f
Nayrax99 <mathieud99@gmail.com>
2026-03-24 13:55:57 +0100

    feat(mobile): ajouter GestureHandlerRootView pour le swipe

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -15,6 +15,7 @@ import {
 import { useEffect } from 'react';
 import { ActivityIndicator, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { AuthProvider, useAuth } from '@/src/contexts/AuthContext';
 import { BusinessProvider } from '@/src/contexts/BusinessContext';
@@ -120,14 +121,16 @@ export default function RootNavigator() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AuthProvider>
-        <BusinessProvider>
-          <RootNavigatorContent />
-          <StatusBar style="auto" />
-        </BusinessProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <AuthProvider>
+          <BusinessProvider>
+            <RootNavigatorContent />
+            <StatusBar style="auto" />
+          </BusinessProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
 
